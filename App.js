3a347c6a09478b1fe6851322e50fa3a8080ba0ba
Meggie Ladlow <meggie@unitedincome.com>
2017-11-11 08:54:44 -0500

    Flow works now

--- App.js
+++ App.js
@@ -2,7 +2,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+type Props = {};
+type State = {};
+
+export default class App extends React.Component<Props, State> {
   render() {
     return (
       <View style={styles.container}>
